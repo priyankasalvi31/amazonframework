7cdffac2444226a08b13438595ab7ad64231f04b
priyanka <psalvi31@gmail.com>
2025-01-03 15:14:22 +0530

    Add shell script outside

---
+++ src/test/java/com/amazon/tests/SampleTest.java
@@ -0,0 +1,12 @@
+package com.amazon.tests;
+
+import java.io.File;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		System.out.println(new File("testRunner/runAutomation.sh").getAbsoluteFile());
+
+	}
+
+}
